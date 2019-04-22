5bd02c96d87ac4d9c2869b4fe040b709f5ce51e1
Chai112 <chayadhana@icloud.com>
2019-04-22 18:53:06 +0700

    Initial Commit

---
+++ ESC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ESC
+//
+//  Created by Chaidhat Chaimongkol on 22/04/2019.
+//  Copyright © 2019 Chai112. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
